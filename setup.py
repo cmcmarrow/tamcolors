d323e922318278f2582411044ad0e1e70e66c9db
cmcmarrow <cmcmarrow@saltstack.com>
2020-07-22 00:24:23 -0600

    remove_3_4_support

--- setup.py
+++ setup.py
@@ -60,7 +60,6 @@ setup(
                  "Programming Language :: C",
                  "Programming Language :: C++",
                  "Programming Language :: Python :: Implementation :: CPython",
-                 "Programming Language :: Python :: 3.4",
                  "Programming Language :: Python :: 3.5",
                  "Programming Language :: Python :: 3.6",
                  "Programming Language :: Python :: 3.7",
