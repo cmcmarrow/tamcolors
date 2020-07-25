1c9e9043adbd57275c64e4af13af383aa027b1ec
cmcmarrow <cmcmarrow@saltstack.com>
2020-07-24 23:48:01 -0600

    rtd work

--- tamcolors/docs/source/conf.py
+++ tamcolors/docs/source/conf.py
@@ -1,5 +1,3 @@
-import sphinx_rtd_theme
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -25,14 +23,14 @@ author = 'Charles McMarrow'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.3'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme"]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,11 +47,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes", ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-master_doc = 'index'
