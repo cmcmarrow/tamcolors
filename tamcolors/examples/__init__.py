fee6ef9f898b9af76c98320818f80dc7c4b1a4b8
cmcmarrow <cmcmarrow@saltstack.com>
2020-08-05 22:18:47 -0600

    add key example

--- tamcolors/examples/__init__.py
+++ tamcolors/examples/__init__.py
@@ -6,6 +6,8 @@ from . import tam_loop
 from . import tam_print
 from . import tam_text_box
 from . import tam_list_buffer
+from . import tam_key_manager
+
 
 __all__ = ("alpha",
            "icon",
@@ -14,5 +16,5 @@ __all__ = ("alpha",
            "tam_loop",
            "tam_print",
            "tam_text_box",
-           "tam_list_buffer")
-
+           "tam_list_buffer",
+           "tam_key_manager")
---
+++ tamcolors/examples/tam_key_manager.py
@@ -0,0 +1,52 @@
+from tamcolors import tam, tam_tools, tam_io
+
+
+class TAMKeyManager(tam.tam_loop.TAMFrame):
+    def __init__(self):
+        super().__init__(fps=10,
+                         char=" ",
+                         foreground_color=tam_io.tam_colors.GRAY,
+                         background_color=tam_io.tam_colors.BLACK,
+                         min_width=70, max_width=70, min_height=40, max_height=40)
+
+        self._key_manager = tam_tools.tam_key_manager.TAMKeyManager()
+        self._output = ""
+
+    def update(self, tam_loop, keys, loop_data):
+        self._key_manager.update(keys)
+
+        if len(self._output) >= 20:
+            self._output = ""
+
+        if self._key_manager.get_key_state("a"):
+            self._output += "a"
+
+        if self._key_manager.get_key_state("b"):
+            self._output += "b"
+
+        if self._key_manager.get_key_state("c"):
+            self._output += "c"
+
+        if self._key_manager.get_key_state("BACKSPACE"):
+            tam_loop.done()
+
+    def draw(self, tam_buffer, loop_data):
+        tam_buffer.clear()
+
+        tam_tools.tam_print.tam_print(tam_buffer,
+                                      0,
+                                      0,
+                                      text=self._output,
+                                      foreground_color=tam_io.tam_colors.LIGHT_GREEN,
+                                      background_color=tam_io.tam_colors.ALPHA)
+
+        tam_tools.tam_print.tam_print(tam_buffer,
+                                      0,
+                                      30,
+                                      text="Try a, b and c.\nbackspace to quit.",
+                                      foreground_color=tam_io.tam_colors.RED,
+                                      background_color=tam_io.tam_colors.ALPHA)
+
+
+def run():
+    tam.tam_loop.TAMLoop(TAMKeyManager()).run()
