7c677e1b2944946b81bdc9ce6b0975fcd7367c1b
cmcmarrow <cmcmarrow@saltstack.com>
2020-10-07 17:15:07 -0600

    fix missing win dll

--- tamcolors/tam_c/__init__.py
+++ tamcolors/tam_c/__init__.py
@@ -1,7 +1,31 @@
+import platform
+import sys
+import os
+
+WIN_VS_DLL_PATH = None
+# get VS dlls path
+if sys.platform == "win32":
+    if platform.architecture()[0] == "32bit":
+        WIN_VS_DLL_PATH = os.path.join(__path__[0], "vs_dlls", "x86")
+    elif platform.architecture()[0] == "64bit":
+        WIN_VS_DLL_PATH = os.path.join(__path__[0], "vs_dlls", "x64")
+    else:
+        WIN_VS_DLL_PATH = os.path.join(__path__[0], "vs_dlls", "arm")
+
 try:
+    # add VS dlls to python path
+    if isinstance(WIN_VS_DLL_PATH, str):
+        sys.path.append(WIN_VS_DLL_PATH)
     from . import _win_tam
 except ImportError:
     _win_tam = None
+finally:
+    # remove VS dlls from path
+    if isinstance(WIN_VS_DLL_PATH, str):
+        try:
+            sys.path.remove(WIN_VS_DLL_PATH)
+        except ValueError:
+            pass
 
 try:
     from . import _uni_tam
