350e5b10751f1769c2274d38afbc6324602f582b
cmcmarrow <charles.mcmarrow.4@gmail.com>
2021-02-22 20:13:41 -0700

    add path tests

---
+++ tamcolors/tests/utils_tests/path_tests.py
@@ -0,0 +1,14 @@
+# built in libraries
+import unittest
+from os.path import join
+
+# tamcolors libraries
+from tamcolors.utils import path
+
+
+class PathTests(unittest.TestCase):
+    def test_abspath(self):
+        self.assertIn("cats", path.abspath("cats"))
+
+    def test_abspath2(self):
+        self.assertIn("cats", path.abspath("cats"))
