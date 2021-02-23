076d42b8732fb4bb01939e4a476489e321b02169
cmcmarrow <charles.mcmarrow.4@gmail.com>
2021-02-22 23:55:16 -0700

    add win sound tests

--- tamcolors/tests/tam_io_tests/any_drivers_tests.py
+++ tamcolors/tests/tam_io_tests/any_drivers_tests.py
@@ -1,6 +1,7 @@
 # built in libraries
 import unittest.mock
 
+
 # tamcolors libraries
 from tamcolors import tam_io
 
@@ -9,7 +10,8 @@ def get_any_io():
     return tam_io.tam_identifier.TAMIdentifier("any_driver_tests",
                                                tam_io.any_drivers.ANYFullColorDriver,
                                                tam_io.any_drivers.ANYKeyDriver,
-                                               tam_io.any_drivers.ANYUtilitiesDriver).get_io()
+                                               tam_io.any_drivers.ANYUtilitiesDriver,
+                                               tam_io.any_drivers.ANYSoundDriver).get_io()
 
 
 class AnyIOTests(unittest.TestCase):
@@ -69,3 +71,17 @@ class AnyIOTests(unittest.TestCase):
     def test_set_tam_color_defaults():
         io = get_any_io()
         io.set_tam_color_defaults()
+
+    def test_sound(self):
+        io = get_any_io()
+        io.open_sound("null.wav", 0)
+        self.assertFalse(io.is_sound_playing(0))
+        io.play_sound(0, False)
+        self.assertFalse(io.is_sound_playing(0))
+        io.pause_sound(0)
+        io.rest_sound(0)
+        self.assertEqual(io.get_sound_position(0), 0)
+        io.set_sound_position(0, 4444)
+        self.assertEqual(io.get_sound_position(0), 0)
+        self.assertEqual(io.get_sound_length(0), 0)
+        io.close_sound(0)
